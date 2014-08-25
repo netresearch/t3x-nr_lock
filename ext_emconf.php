1d83361c87a997a136014c21743c7907aa3f5d01
Sebastian Mendel <sebastian.mendel@netresearch.de>
2014-08-25 14:17:28 +0200

    Version: 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "nr_lock".
  *
- * Auto generated 31-07-2014 13:27
+ * Auto generated 25-08-2014 14:17
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 	),
 	'state' => 'stable',
-	'version' => '1.0.1',
-	'_md5_values_when_last_written' => 'a:10:{s:9:"build.xml";s:4:"3f95";s:9:"ChangeLog";s:4:"8da3";s:16:"ext_autoload.php";s:4:"99e3";s:17:"ext_localconf.php";s:4:"6fe2";s:10:"README.rst";s:4:"355a";s:18:"doc/ide_helper.php";s:4:"02db";s:12:"src/Lock.php";s:4:"6a79";s:24:"src/Backend/Abstract.php";s:4:"70ae";s:25:"src/Backend/Couchbase.php";s:4:"8dd7";s:21:"src/Backend/Redis.php";s:4:"75e0";}',
+	'version' => '2.0.0',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"build.xml";s:4:"3f95";s:9:"ChangeLog";s:4:"bea1";s:16:"ext_autoload.php";s:4:"8322";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"aa1d";s:10:"README.rst";s:4:"bf9c";s:18:"doc/ide_helper.php";s:4:"02db";s:12:"src/Lock.php";s:4:"0c7f";s:24:"src/Backend/Abstract.php";s:4:"a243";s:25:"src/Backend/Couchbase.php";s:4:"f6a0";s:21:"src/Backend/Redis.php";s:4:"1b76";s:19:"tests/bootstrap.php";s:4:"755b";s:18:"tests/LockTest.php";s:4:"5501";s:17:"tests/phpunit.xml";s:4:"c77f";s:30:"tests/Backend/AbstractTest.php";s:4:"26eb";s:27:"tests/Backend/RedisTest.php";s:4:"486d";}',
 	'suggests' => array(
 	),
 );
